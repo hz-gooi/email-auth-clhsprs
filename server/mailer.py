6beeb5183dc2fb443099a7fe9c96e3f04eb638c8
Gooi Hean Zhen <m-7706114@moe-dl.edu.my>
2023-07-13 19:04:52 +0800

    Update mailer.py

--- server/mailer.py
+++ server/mailer.py
@@ -26,7 +26,7 @@ try:
 
 
     port = os.getenv("mailPort")
-    FROM_MAIL = "no-reply@clhsprs.com"
+    FROM_MAIL = OrgMail
     TO_MAIL = sys.argv[1]
     OTP = sys.argv[3]
     COMPANY_NAME = "CLHS Peer Counselling Unit"
