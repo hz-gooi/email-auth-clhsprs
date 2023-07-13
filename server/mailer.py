46ed7eae3b885d363cc4331f933ac4fd54cdbd8c
Gooi Hean Zhen <m-7706114@moe-dl.edu.my>
2023-07-13 18:55:40 +0800

    Update mailer.py

--- server/mailer.py
+++ server/mailer.py
@@ -26,15 +26,11 @@ try:
 
 
     port = os.getenv("mailPort")
-    FROM_MAIL = OrgMail
-    TO_MAIL = sys.argv[2]
+    FROM_MAIL = "no-reply@clhsprs.com"
+    TO_MAIL = sys.argv[1]
     OTP = sys.argv[3]
     COMPANY_NAME = "CLHS Peer Counselling Unit"
-    try:
-        COMPANY_NAME = "CLHS Peer Counselling Unit"
-    except IndexError:
-        print("No company name present")
-        COMPANY_NAME = ""
+    
 
     # Create the HTML file
     HTML = ""
