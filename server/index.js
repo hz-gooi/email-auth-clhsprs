a4ebd590b5031f23094e4a657224e0c5ea0b4a57
Gooi Hean Zhen <m-7706114@moe-dl.edu.my>
2024-07-26 17:10:34 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,35 +1,35 @@
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
 
 // Server essentials
 const cors = require('cors');
 const helmet = require('helmet');
 
-// environment configurations
+// Environment configurations
 const dotenv = require('dotenv');
 dotenv.config({
-    path: path.resolve("../", "custom", "config.env")
+    path: path.resolve(__dirname, '../custom/config.env')
 });
 
-
-// server configurations
-
+// Server configurations
 app.use(helmet());
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// router for the server
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Router for the server
 const router = require('./routes/route');
 app.use('/', router);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('server started');
     console.log('port', port);
 });
