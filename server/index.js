c80f954a4603e232a7c867579b7a0d201f875ed1
Gooi Hean Zhen <m-7706114@moe-dl.edu.my>
2024-07-26 20:02:32 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { spawn } = require('child_process'); // Import spawn
+
 
 const app = express();
 
@@ -14,9 +14,12 @@ dotenv.config({
     path: path.resolve("../", "custom", "config.env")
 });
 
+
 // server configurations
+
 app.use(helmet());
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -26,29 +29,7 @@ app.use('/', router);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+app.listen(port, ()=>{
     console.log('server started');
     console.log('port', port);
-});
-
-// Assuming email and otp variables are defined somewhere in your code
-const pythonProcess = spawn('python3', [
-    'server/mailer.py',
-    'dart',
-    email,
-    otp,
-    'Sample session'
-]);
-
-// Handle the process output and errors
-pythonProcess.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-});
-
-pythonProcess.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-});
-
-pythonProcess.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
 });
